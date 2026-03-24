d502db280a3ce4e45f9c9f2e919ef4c6e875c263
ThemeHunk <themehunk@gmail.com>
2026-03-24 17:58:27 +0530

    meger file

--- build/admin/index.asset.php
+++ build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '36de88034a0ec2f2e22d');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9349531405124e25518a');
